ab5a8cdb3984e8b438ba718f451be1dd36a974fa
Aidan Heller <asfheller@gmail.com>
2018-07-17 09:00:02 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.gradle.internal.impldep.org.apache.maven.model.Build
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+project.apply { from("./versions.gradle.kts") }
+
+val kotlinVrs: String by extra
+val ardulinkVrs: String by extra
+val tornadoFxVrs: String by extra
+
+plugins {
+    java
+    kotlin("jvm") version "1.2.60-eap-7"
+    application
+}
+
+group = "com.jamdan2.ServoTrajectoryGui"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
+}
+
+dependencies {
+    kotlin("").toString()
+    compile(kotlin("stdlib-jdk8", kotlinVrs))
+    compile("org.ardulink", "ardulink-core-base", ardulinkVrs)
+    compile("org.ardulink", "ardulink-core-serial-jssc", ardulinkVrs)
+    compile("no.tornado", "tornadofx", tornadoFxVrs)
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Main-Class"] = "com.company.servotrajectorygui.MainKt"
+    }
+    from(configurations.runtime.map {
+        @Suppress("IMPLICIT_CAST_TO_ANY")
+        if (it.isDirectory) it else zipTree(it)
+    })
+    with(tasks["jar"] as CopySpec)
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    "build" {
+        dependsOn(fatJar)
+    }
+}
+
+application {
+    mainClassName = "com.company.servotrajectorygui.MainKt"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        jcenter()
+        maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
+        maven { setUrl("https://plugins.gradle.org/m2/") }
+    }
+}
+rootProject.name = "ServoTrajectoryGui"
