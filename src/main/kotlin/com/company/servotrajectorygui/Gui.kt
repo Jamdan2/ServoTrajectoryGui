cedc20c5d19619ed526e88d3c7419962a7ab7143
Aidan Heller <asfheller@gmail.com>
2018-07-17 10:49:01 -0700

    Improve gui

--- src/main/kotlin/com/company/servotrajectorygui/Gui.kt
+++ src/main/kotlin/com/company/servotrajectorygui/Gui.kt
@@ -1,13 +1,21 @@
 package com.company.servotrajectorygui
 
+import javafx.stage.Stage
 import tornadofx.*
 
-class Gui : App(MainView::class)
+class Gui : App(MainView::class, Style::class) {
+    override fun start(stage: Stage) {
+        super.start(stage)
+        stage.isResizable = false
+    }
+}
 
 class MainView : View() {
     override val root = vbox {
-        padding = insets(10)
+        title = "Servo Trajectory Gui"
+        addClass(Style.wrapper)
         hbox {
+            addClass(Style.wrapper)
             button {
                 text = "Reset servo"
             }
@@ -16,33 +24,57 @@ class MainView : View() {
             }
         }
         vbox {
-            label("Distance")
-            slider {
-                min = 0.0
-                max = 180.0
-                minorTickCount = 30
-                majorTickUnit = 15.0
-                isShowTickMarks = true
-                isShowTickLabels = true
+            addClass(Style.wrapper)
+            vbox {
+                label("Distance")
+                slider {
+                    min = 0.0
+                    max = 180.0
+                    minorTickCount = 30
+                    majorTickUnit = 15.0
+                    isShowTickMarks = true
+                    isShowTickLabels = true
+                }
             }
-            label("Max Velocity")
-            slider {
-                min = 0.0
-                max = 180.0
-                minorTickCount = 30
-                majorTickUnit = 15.0
-                isShowTickMarks = true
-                isShowTickLabels = true
+            vbox {
+                label("Max Velocity")
+                slider {
+                    min = 0.0
+                    max = 180.0
+                    minorTickCount = 30
+                    majorTickUnit = 15.0
+                    isShowTickMarks = true
+                    isShowTickLabels = true
+                }
             }
-            label("Max Acceleration")
-            slider {
-                min = 0.0
-                max = 180.0
-                minorTickCount = 30
-                majorTickUnit = 15.0
-                isShowTickMarks = true
-                isShowTickLabels = true
+            vbox {
+                label("Max Acceleration")
+                slider {
+                    min = 0.0
+                    max = 180.0
+                    minorTickCount = 30
+                    majorTickUnit = 15.0
+                    isShowTickMarks = true
+                    isShowTickLabels = true
+                }
             }
+        }
+    }
+}
+
+class Style : Stylesheet() {
+    companion object {
+        val wrapper by cssclass()
+    }
+    init {
+        root {
+            padding = box(5.mm)
+        }
+        slider {
+            prefWidth = 150.mm
+        }
+        wrapper {
+            spacing = 3.mm
         }
     }
 }
