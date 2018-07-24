56aaa44916d98539eaac837be17ad653e7823fe4
Aidan Heller <asfheller@gmail.com>
2018-07-24 13:54:45 -0700

    Add graph for distance points

--- src/main/kotlin/com/company/servotrajectorygui/Gui.kt
+++ src/main/kotlin/com/company/servotrajectorygui/Gui.kt
@@ -1,9 +1,17 @@
 package com.company.servotrajectorygui
 
+import javafx.event.Event
 import javafx.event.EventHandler
+import javafx.event.EventType
+import javafx.scene.chart.Axis
+import javafx.scene.chart.CategoryAxis
+import javafx.scene.chart.NumberAxis
+import javafx.scene.control.Button
 import javafx.stage.Stage
 import tornadofx.*
 import kotlin.math.roundToInt
+
+object TrajectoryRecalculated : FXEvent(EventBus.RunOn.ApplicationThread)
 
 class Gui : App(MainView::class, Style::class) {
     override fun start(stage: Stage) {
@@ -13,94 +21,113 @@ class Gui : App(MainView::class, Style::class) {
 }
 
 class MainView : View() {
-    override val root = vbox {
+    override val root = hbox {
         title = "Servo Trajectory Gui"
         addClass(Style.wrapper)
-        hbox {
-            addClass(Style.wrapper)
-            button {
-                text = "Reset servo"
-                action {
-                    link.setServoAngle(0)
+        vbox {
+            hbox {
+                addClass(Style.wrapper)
+                button {
+                    text = "Reset servo"
+                    action {
+                        link.setServoAngle(0)
+                    }
+                }
+                button {
+                    text = "Calculate trajectory"
+                    action {
+                        reinitializeTrajectory()
+                        trajectory.calculateDistancePoints()
+                        if (!trajectory.isValid) warning("trajectory is invalid")
+                        fire(TrajectoryRecalculated)
+                    }
+                }
+                button {
+                    text = "Run trajectory"
+                    action {
+                        if (trajectory.isValid) trajectory.distancePoints.iterator().forEach {
+                            link.setServoAngle(it.roundToInt())
+                            Thread.sleep(1)
+                        } else error("trajectory is invalid")
+                    }
                 }
             }
-            button {
-                text = "Calculate trajectory"
-                action {
-                    reinitializeTrajectory()
-                    trajectory.calculateDistancePoints()
-                    if (!trajectory.isValid) warning("trajectory is invalid")
+            vbox {
+                addClass(Style.wrapper)
+                vbox {
+                    val distanceLabel = label("Distance: 90")
+                    slider(0, 180, 90) {
+                        minorTickCount = 15
+                        majorTickUnit = 15.0
+                        isShowTickMarks = true
+                        isShowTickLabels = true
+                        valueProperty().addListener { _, _, newValue ->
+                            distanceLabel.text = "Distance: ${newValue.toInt()}"
+                        }
+                        onMouseReleased = EventHandler {
+                            setDistance(value.toInt())
+                        }
+                    }
                 }
-            }
-            button {
-                text = "Run trajectory"
-                action {
-                    if (trajectory.isValid) trajectory.distancePoints.iterator().forEach {
-                        link.setServoAngle(it.roundToInt())
-                        Thread.sleep(1)
-                    } else error("trajectory is invalid")
+                vbox {
+                    val velocityLabel = label("Max Velocity: 0.5")
+                    slider(0, 1, 0.5) {
+                        minorTickCount = 10
+                        majorTickUnit = 0.1
+                        isShowTickMarks = true
+                        isShowTickLabels = true
+                        valueProperty().addListener { _, _, newValue ->
+                            velocityLabel.text = "Max Velocity: ${newValue.toDouble()}"
+                        }
+                        onMouseReleased = EventHandler {
+                            setVelocity(value)
+                        }
+                    }
+                }
+                vbox {
+                    val accelerationLabel = label("Max Acceleration: 0.005")
+                    slider(0, 0.01, 0.005) {
+                        minorTickCount = 10
+                        majorTickUnit = 0.001
+                        isShowTickMarks = true
+                        isShowTickLabels = true
+                        valueProperty().addListener { _, _, newValue ->
+                            accelerationLabel.text = "Max Acceleration: ${newValue.toDouble()}"
+                        }
+                        onMouseReleased = EventHandler {
+                            setAcceleration(value)
+                        }
+                    }
+                }
+                vbox {
+                    val jerkLabel = label("Max Jerk: 0.005")
+                    slider(0, 0.01, 0.005) {
+                        minorTickCount = 10
+                        majorTickUnit = 0.1
+                        isShowTickMarks = true
+                        isShowTickLabels = true
+                        valueProperty().addListener { _, _, newValue ->
+                            jerkLabel.text = "Max Jerk: ${newValue.toDouble()}"
+                        }
+                        onMouseReleased = EventHandler {
+                            setJerk(value)
+                        }
+                    }
                 }
             }
         }
         vbox {
-            addClass(Style.wrapper)
-            vbox {
-                val distanceLabel = label("Distance: 90")
-                slider(0, 180, 90) {
-                    minorTickCount = 15
-                    majorTickUnit = 15.0
-                    isShowTickMarks = true
-                    isShowTickLabels = true
-                    valueProperty().addListener { _, _, newValue ->
-                        distanceLabel.text = "Distance: ${newValue.toInt()}"
+            linechart("Calculated trajectory", NumberAxis(), NumberAxis()) {
+                series("Distance") {
+                    trajectory.calculateDistancePoints()
+                    trajectory.distancePoints.forEachIndexed { index, value ->
+                        data(index, value)
                     }
-                    onMouseReleased = EventHandler {
-                        setDistance(value.toInt())
-                    }
-                }
-            }
-            vbox {
-                val velocityLabel = label("Max Velocity: 0.5")
-                slider(0, 1, 0.5) {
-                    minorTickCount = 10
-                    majorTickUnit = 0.1
-                    isShowTickMarks = true
-                    isShowTickLabels = true
-                    valueProperty().addListener { _, _, newValue ->
-                        velocityLabel.text = "Max Velocity: ${newValue.toDouble()}"
-                    }
-                    onMouseReleased = EventHandler {
-                        setVelocity(value)
-                    }
-                }
-            }
-            vbox {
-                val accelerationLabel = label("Max Acceleration: 0.005")
-                slider(0, 0.01, 0.005) {
-                    minorTickCount = 10
-                    majorTickUnit = 0.001
-                    isShowTickMarks = true
-                    isShowTickLabels = true
-                    valueProperty().addListener { _, _, newValue ->
-                        accelerationLabel.text = "Max Acceleration: ${newValue.toDouble()}"
-                    }
-                    onMouseReleased = EventHandler {
-                        setAcceleration(value)
-                    }
-                }
-            }
-            vbox {
-                val jerkLabel = label("Max Jerk: 0.005")
-                slider(0, 0.01, 0.005) {
-                    minorTickCount = 10
-                    majorTickUnit = 0.1
-                    isShowTickMarks = true
-                    isShowTickLabels = true
-                    valueProperty().addListener { _, _, newValue ->
-                        jerkLabel.text = "Max Jerk: ${newValue.toDouble()}"
-                    }
-                    onMouseReleased = EventHandler {
-                        setJerk(value)
+                    subscribe<TrajectoryRecalculated> {
+                        data.clear()
+                        trajectory.distancePoints.forEachIndexed { index, value ->
+                            data(index, value)
+                        }
                     }
                 }
             }
