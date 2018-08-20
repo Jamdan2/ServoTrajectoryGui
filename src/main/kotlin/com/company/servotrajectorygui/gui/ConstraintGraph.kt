791466324b3c2708655a9011c531f66863b0bde9
Aidan Heller <asfheller@gmail.com>
2018-08-20 11:19:48 -0700

    Fix bug with jerk graph

--- src/main/kotlin/com/company/servotrajectorygui/gui/ConstraintGraph.kt
+++ src/main/kotlin/com/company/servotrajectorygui/gui/ConstraintGraph.kt
@@ -1,63 +1,17 @@
 package com.company.servotrajectorygui.gui
 
+import com.company.servotrajectorygui.roundTo
+import com.company.servotrajectorygui.trajectory.a
 import com.company.servotrajectorygui.virtualTimer
 import javafx.scene.chart.NumberAxis
 import javafx.scene.chart.XYChart
 import tornadofx.*
 
 class ConstraintGraph : Fragment() {
-    private val trajectoryController by inject<TrajectoryController>()
-
     private val constraintName: String? by params
-    private val constraintFunction: ((Double) -> Double)? by params
     private val graphStyles: CssRule? by params
-    private val graphingMethod: GraphingMethod? by params
     private val constraintUnit: String? by params
-
-    enum class GraphingMethod {
-        POINT_ITERATION,
-        VITAL_POINT,
-        VERTICAL_SUPPORT
-    }
-
-    private fun XYChart.Series<Number, Number>.graph() {
-        data.clear()
-        when (graphingMethod!!) {
-            GraphingMethod.POINT_ITERATION -> {
-                virtualTimer(trajectoryController.trajectory.t7, trajectoryController.trajectory.t7 / 150) {
-                    data(it, constraintFunction!!(it))
-                }
-                data(trajectoryController.trajectory.t7, constraintFunction!!(trajectoryController.trajectory.t7))
-            }
-            GraphingMethod.VITAL_POINT -> {
-                data(0.0, constraintFunction!!(0.0))
-                with(trajectoryController.trajectory.t1) { data(this, constraintFunction!!(this)) }
-                with(trajectoryController.trajectory.t2) { data(this, constraintFunction!!(this)) }
-                with(trajectoryController.trajectory.t3) { data(this, constraintFunction!!(this)) }
-                with(trajectoryController.trajectory.t4) { data(this, constraintFunction!!(this)) }
-                with(trajectoryController.trajectory.t5) { data(this, constraintFunction!!(this)) }
-                with(trajectoryController.trajectory.t6) { data(this, constraintFunction!!(this)) }
-                with(trajectoryController.trajectory.t7) { data(this, constraintFunction!!(this)) }
-            }
-            GraphingMethod.VERTICAL_SUPPORT -> {
-                data(0.0, constraintFunction!!(0.0))
-                with(trajectoryController.trajectory.t1) { data(0.0, constraintFunction!!(this)) }
-                with(trajectoryController.trajectory.t1) { data(this, constraintFunction!!(this)) }
-                data(trajectoryController.trajectory.t1, constraintFunction!!(trajectoryController.trajectory.t2))
-                with(trajectoryController.trajectory.t2) { data(this, constraintFunction!!(this)) }
-                data(trajectoryController.trajectory.t2, constraintFunction!!(trajectoryController.trajectory.t3))
-                with(trajectoryController.trajectory.t3) { data(this, constraintFunction!!(this)) }
-                data(trajectoryController.trajectory.t3, constraintFunction!!(trajectoryController.trajectory.t4))
-                with(trajectoryController.trajectory.t4) { data(this, constraintFunction!!(this)) }
-                data(trajectoryController.trajectory.t4, constraintFunction!!(trajectoryController.trajectory.t5))
-                with(trajectoryController.trajectory.t5) { data(this, constraintFunction!!(this)) }
-                data(trajectoryController.trajectory.t5, constraintFunction!!(trajectoryController.trajectory.t6))
-                with(trajectoryController.trajectory.t6) { data(this, constraintFunction!!(this)) }
-                data(trajectoryController.trajectory.t6, constraintFunction!!(trajectoryController.trajectory.t7))
-                with(trajectoryController.trajectory.t7) { data(this, constraintFunction!!(this)) }
-            }
-        }
-    }
+    private val graphingFunction: (XYChart.Series<Number, Number>.() -> Unit)? by params
 
     override val root = linechart(constraintName!!, NumberAxis(), NumberAxis()) {
         addClass(graphStyles!!)
@@ -68,8 +22,11 @@ class ConstraintGraph : Fragment() {
         xAxis.label = "time"
         yAxis.label = constraintUnit!!
         series(constraintName!!) {
-            graph()
-            subscribe<TrajectoryRecalculated> { graph() }
+            graphingFunction!!()
+            subscribe<TrajectoryRecalculated> {
+                data.clear()
+                graphingFunction!!()
+            }
         }
     }
 }
--- src/main/kotlin/com/company/servotrajectorygui/gui/GraphView.kt
+++ src/main/kotlin/com/company/servotrajectorygui/gui/GraphView.kt
@@ -1,5 +1,6 @@
 package com.company.servotrajectorygui.gui
 
+import com.company.servotrajectorygui.roundTo
 import com.company.servotrajectorygui.trajectory.a
 import com.company.servotrajectorygui.trajectory.d
 import com.company.servotrajectorygui.trajectory.j
@@ -7,42 +8,94 @@ import com.company.servotrajectorygui.trajectory.v
 import com.company.servotrajectorygui.virtualTimer
 import javafx.scene.chart.NumberAxis
 import javafx.scene.chart.XYChart
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
 import tornadofx.*
 import java.util.*
 
 class GraphView : View() {
     private val trajectoryController by inject<TrajectoryController>()
 
+    private val graphDistance: XYChart.Series<Number, Number>.() -> Unit = {
+        virtualTimer(trajectoryController.trajectory.t7, trajectoryController.trajectory.t7 / 150) {
+            data(it, trajectoryController.trajectory.d(it))
+        }
+        data(trajectoryController.trajectory.t7, trajectoryController.trajectory.d(trajectoryController.trajectory.t7))
+    }
+
+    private val graphVelocity: XYChart.Series<Number, Number>.() -> Unit = {
+        virtualTimer(trajectoryController.trajectory.t7, trajectoryController.trajectory.t7 / 150) {
+            data(it, trajectoryController.trajectory.v(it))
+        }
+        data(trajectoryController.trajectory.t7, trajectoryController.trajectory.v(trajectoryController.trajectory.t7))
+    }
+
+    private val graphAcceleration: XYChart.Series<Number, Number>.() -> Unit = {
+        with(trajectoryController.trajectory) {
+            data(0.0, a(0.0))
+            data(t1, a(t1))
+            data(t2, a(t2))
+            data(t3, a(t3))
+            data(t4, a(t4))
+            data(t5, a(t5))
+            data(t6, a(t6))
+            data(t7, a(t7))
+        }
+    }
+
+    private val graphJerk: XYChart.Series<Number, Number>.() -> Unit = {
+        with(trajectoryController.trajectory) {
+            data(0.0, 0.0)
+            data(0.0, j)
+            data(t1.roundTo(8), j)
+            if (t1.roundTo(8) != t2.roundTo(8)) {
+                data(t1.roundTo(8), 0.0)
+                data(t2.roundTo(8), 0.0)
+            }
+            data(t2.roundTo(8), -j)
+            data(t3.roundTo(8), -j)
+            if (t3.roundTo(8) != t4.roundTo(8)) {
+                data(t3.roundTo(8), 0.0)
+                data(t4.roundTo(8), 0.0)
+            }
+            data(t4.roundTo(8), -j)
+            data(t5.roundTo(8), -j)
+            if (t5.roundTo(8) != t6.roundTo(8)) {
+                data(t5.roundTo(8), 0.0)
+                data(t6.roundTo(8), 0.0)
+            }
+            data(t6.roundTo(8), j)
+            data(t7.roundTo(8), j)
+            data(t7.roundTo(8), 0.0)
+        }
+    }
+
     private val distanceGraph = find<ConstraintGraph>(
             "constraintName" to "distance",
-            "constraintFunction" to { t: Double -> trajectoryController.trajectory.d(t) },
             "graphStyles" to Styles.distanceChart,
-            "graphingMethod" to ConstraintGraph.GraphingMethod.POINT_ITERATION,
-            "constraintUnit" to "rotations"
+            "constraintUnit" to "rotations",
+            "graphingFunction" to graphDistance
     )
 
     private val velocityGraph = find<ConstraintGraph>(
             "constraintName" to "velocity",
-            "constraintFunction" to { t: Double -> trajectoryController.trajectory.v(t) },
             "graphStyles" to Styles.velocityChart,
-            "graphingMethod" to ConstraintGraph.GraphingMethod.POINT_ITERATION,
-            "constraintUnit" to "rotations / second"
+            "constraintUnit" to "rotations / second",
+            "graphingFunction" to graphVelocity
     )
 
     private val accelerationGraph = find<ConstraintGraph>(
             "constraintName" to "acceleration",
-            "constraintFunction" to { t: Double -> trajectoryController.trajectory.a(t) },
             "graphStyles" to Styles.accelerationChart,
-            "graphingMethod" to ConstraintGraph.GraphingMethod.VITAL_POINT,
-            "constraintUnit" to "rotations / second^2"
+            "constraintUnit" to "rotations / second^2",
+            "graphingFunction" to graphAcceleration
     )
 
     private val jerkGraph = find<ConstraintGraph>(
             "constraintName" to "jerk",
-            "constraintFunction" to { t: Double -> trajectoryController.trajectory.j(t) },
             "graphStyles" to Styles.jerkChart,
-            "graphingMethod" to ConstraintGraph.GraphingMethod.VERTICAL_SUPPORT,
-            "constraintUnit" to "rotations / second^3"
+            "constraintUnit" to "rotations / second^3",
+            "graphingFunction" to graphJerk
     )
 
     override val root = vbox {
